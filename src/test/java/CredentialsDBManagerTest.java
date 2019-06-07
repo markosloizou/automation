51ccedc328cbd11585c80596ed9d445f614adeb4
Markos Loizou <markosloizou@hotmail.com>
2019-06-07 18:27:35 +0100

    NEW TESTER FOR DATABASEMANAGER
    Tests if table exists, test adding ftp user and also tests getting the
    ftp test user

--- src/test/java/CredentialsDBManagerTest.java
+++ src/test/java/CredentialsDBManagerTest.java
@@ -10,5 +10,30 @@ public class CredentialsDBManagerTest extends  TestCase{
         assertEquals(DBM.tableExists(), true);
     }
 
+    @Test
+    public void TestFTPUser(){
+        String password = "5p2tvn92R0di8FdiLCfzeeT0b";
+
+        AgencyCredentials AC = new AgencyCredentials(Agency.TEST, "dlpuser@dlptest.com", password.toCharArray(),"dlpuser@dlptest.com", password.toCharArray());
+
+        assertEquals(true,DBM.saveFTPcredentials(AC));
+    }
+
+    @Test
+    public void GetTestFTPUser()
+    {
+        String password = "5p2tvn92R0di8FdiLCfzeeT0b";
+        AgencyCredentials AC_true = new AgencyCredentials(Agency.TEST, "dlpuser@dlptest.com", password.toCharArray(),"dlpuser@dlptest.com", password.toCharArray());
+
+        AgencyCredentials AC = DBM.getFTPcredentials(Agency.TEST);
+
+
+        assertEquals(AC_true.getAgency().getCode(), AC.getAgency().getCode());
+        assertEquals(AC_true.getUname(), AC.getUname());
+        assertEquals(AC_true.getPwd(), AC.getPwd());
+        assertEquals(AC_true.getFTPuname(), AC.getFTPuname());
+        assertEquals(AC_true.getFTPpwd(), AC.getFTPpwd());
+    }
+
 
 }
