e39354e0523f2690f8d40ab328ef9632bfeb9f32
Markos Loizou <markosloizou@hotmail.com>
2019-06-07 18:22:44 +0100

    ADDED HASHING FOR EACH ENUM VALUE AND A TEST AGENCY
    Each agency is mapped to one integer in order to store it in the
    sqlite database.
    
    A test agency pointing to https://dlptest.com/ftp-test/ has been added.
    This website allows uploading tests. Pitfal password changes relatively
    frequently. Need to keep it up to date.

--- src/main/java/Agency.java
+++ src/main/java/Agency.java
@@ -1,9 +1,34 @@
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Agency {
-    SHUTTERSTOCK,
-    DREAMSTIME,
-    ALAMY,
-    DEPOSITPHOTOS,
-    CANSTOCKPHOTOS,
-    POND5,
-    ADOBESTOCK
+    SHUTTERSTOCK(0),
+    DREAMSTIME(1),
+    ALAMY(2),
+    DEPOSITPHOTOS(3),
+    CANSTOCKPHOTOS(4),
+    POND5(4),
+    ADOBESTOCK(5),
+    TEST(100);
+
+    private static final Map<Integer,Agency> lookup
+            = new HashMap<Integer,Agency>();
+
+    static {
+        for(Agency a: EnumSet.allOf(Agency.class))
+            lookup.put(a.getCode(), a);
+    }
+
+    private int code;
+
+    private Agency(int code) {
+        this.code = code;
+    }
+
+    public int getCode() { return code; }
+
+    public static Agency get(int code) {
+        return lookup.get(code);
+    }
 }
