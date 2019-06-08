b4781d894f67e307e9e35f47cbd5367bed6c0e43
Markos Loizou <markosloizou@hotmail.com>
2019-06-08 22:33:23 +0100

    NEW RUNNER FOR DBMANAGER TESTS

--- src/test/java/CredentialsDBMManagerTestRunner.java
+++ src/test/java/CredentialsDBMManagerTestRunner.java
@@ -1,7 +1,10 @@
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.RunWith;
 import org.junit.runner.notification.Failure;
+import org.junit.runners.Suite;
+
 
 
 public class CredentialsDBMManagerTestRunner {
