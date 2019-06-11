03a7d7192365df99fb630a71cb43718201506d50
Markos Loizou <markosloizou@hotmail.com>
2019-06-11 13:48:11 +0100

    ADDED TEST TO TEST GETAGGENCYCREDENTIALS

--- src/test/java/CredentialsDBManagerTest.java
+++ src/test/java/CredentialsDBManagerTest.java
@@ -53,5 +53,20 @@ public class CredentialsDBManagerTest extends  TestCase{
         assertEquals(new String(AC_true.getFTPpwd()), new String(AC.getFTPpwd()));
     }
 
+    @Test
+    public void testGetAgencyUsertest()
+    {
+        String password = "5p2tvn92R0di8FdiLCfzeeT0b";
+        AgencyCredentials AC_true = new AgencyCredentials(Agency.TEST, "dlpuser@dlptest.com", password.toCharArray(),"dlpuser@dlptest.com", password.toCharArray());
+
+        AgencyCredentials AC = DBM.getAgencyCredentials(Agency.TEST);
+
+
+        assertEquals(AC_true.getAgency().getCode(), AC.getAgency().getCode());
+        assertEquals(AC_true.getUname(), AC.getUname());
+        assertEquals(new String(AC_true.getPwd()), new String(AC.getPwd()));
+        assertEquals(AC_true.getFTPuname(), AC.getFTPuname());
+        assertEquals(null, AC.getFTPpwd());
+    }
 
 }
