9fb6475cf56c405e1bd84c844378e17ef87b0da6
Markos Loizou <markosloizou@hotmail.com>
2019-06-23 18:04:50 +0100

    REMOVED GUI INITIALIZATION IN CLASS, MOVED EVERYTHING TO GUI CLASS

--- src/main/java/FileTree.java
+++ src/main/java/FileTree.java
@@ -2,13 +2,15 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.event.*;
 import javax.swing.filechooser.FileSystemView;
+import javax.swing.plaf.metal.MetalLookAndFeel;
 import javax.swing.tree.*;
 import java.awt.*;
 import java.io.File;
 import java.io.FilenameFilter;
+
 import java.util.Arrays;
 
-public class FileTree extends JFrame implements TreeSelectionListener, TreeWillExpandListener
+public class FileTree implements TreeSelectionListener, TreeWillExpandListener
 {
     private JTree tree;
 
@@ -33,20 +35,30 @@ public class FileTree extends JFrame implements TreeSelectionListener, TreeWillE
             addChildren(node);
         }
 
-        Container content = this.getContentPane();
-        content.setBackground(Color.DARK_GRAY);
 
-        DefaultTreeModel treeModel = new DefaultTreeModel(root,true);
+
+        Icons pair = new Icons().getMetal();
+
+        UIManager.put("Tree.collapsedIcon", pair.collapsed);
+        UIManager.put("Tree.expandedIcon", pair.expanded);
+
+        //DefaultTreeModel treeModel = new DefaultTreeModel(root,true);
+        DefaultTreeModel treeModel = new DefaultTreeModel(root);
+
         tree = new JTree(treeModel);
-        JScrollPane scrollPane = new JScrollPane(tree);
-        content.add(scrollPane);
+//        JScrollPane scrollPane = new JScrollPane(tree);
+//        content.add(scrollPane);
         //add(scrollPane);
         //Container.add(scrollPane, Integer.parseInt(BorderLayout.CENTER));
 
-        scrollPane.getViewport().setOpaque(false);
-        scrollPane.setOpaque(false);
+//        scrollPane.getViewport().setOpaque(false);
+//        scrollPane.setOpaque(false);
 
+        try{
         tree.setEditable(true);
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
         tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         tree.setShowsRootHandles(true);
         tree.setOpaque(false);
@@ -56,14 +68,18 @@ public class FileTree extends JFrame implements TreeSelectionListener, TreeWillE
         tree.addTreeSelectionListener(this);
         tree.addTreeWillExpandListener(this);
         tree.setCellRenderer(new MyCellRenderer());
-
+/*
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setBackground(Color.darkGray);
         this.setForeground(Color.gray);
         this.setTitle("JTree Example");
         this.pack();
         this.setSize(400,600);
-        this.setVisible(true);
+        this.setVisible(true);*/
+    }
+
+    public JTree getTree(){
+        return  tree;
     }
 
     public void valueChanged(TreeSelectionEvent e) {
@@ -76,16 +92,18 @@ public class FileTree extends JFrame implements TreeSelectionListener, TreeWillE
         if (node == null)
             //Nothing is selected.
             return;
-
+*/
         Object nodeInfo = node.getUserObject();
         TreeNode[] path = node.getPath();
         String filePath = "";
         for(TreeNode tn: path) {
-            if(tn.toString() == "root") continue;
+            if(tn.toString().equals("root")) continue;
+            else if(tn.toString().equals("/")) continue;
             else{
                 filePath += "/" + tn.toString();
             }
         }
+        /*
         File selected = new File(filePath);
 
 
@@ -100,17 +118,17 @@ public class FileTree extends JFrame implements TreeSelectionListener, TreeWillE
         }
 
 */
-        System.out.println("Selected: " + node.toString());
+        System.out.println("Selected: " + node.toString() + "\t Path: " + filePath );
 
 
     }
 
     private void LoadLazyChildren(TreeExpansionEvent e){
 
-        DefaultMutableTreeNode last = (DefaultMutableTreeNode)e.getPath().getLastPathComponent();
+        DefaultMutableTreeNode last = (DefaultMutableTreeNode)e.getPath().getLastPathComponent(); //gets component that will expand
         for(int i = 0; i < tree.getModel().getChildCount(last); i++){
-            if(tree.getModel().getChildCount( tree.getModel().getChild(last, i)) > 0) continue;
-            addChildren((DefaultMutableTreeNode) tree.getModel().getChild(last, i));
+            if(tree.getModel().getChildCount( tree.getModel().getChild(last, i)) > 0) continue; //if it already has children continue
+            addChildren((DefaultMutableTreeNode) tree.getModel().getChild(last, i)); //if no children add the children
         }
         System.out.println("Expanding Node " + e.getPath().getLastPathComponent());
     }
@@ -121,7 +139,7 @@ public class FileTree extends JFrame implements TreeSelectionListener, TreeWillE
         TreeNode[] path = node.getPath();
         String filePath = "";
         for(TreeNode tn: path) {
-            if(tn.toString() == "root") continue;
+            if(tn.toString().equals("root")) continue;
             else{
                 filePath += "/" + tn.toString();
             }
@@ -158,6 +176,7 @@ public class FileTree extends JFrame implements TreeSelectionListener, TreeWillE
 
     }
 
+    /*
     public static void main(String[] args)
     {
         SwingUtilities.invokeLater(new Runnable() {
@@ -166,7 +185,7 @@ public class FileTree extends JFrame implements TreeSelectionListener, TreeWillE
                 new FileTree();
             }
         });
-    }
+    }*/
 
     private String[] getSubdirectories(File file){
     String[] directories = file.list(new FilenameFilter() {
@@ -206,6 +225,31 @@ public class FileTree extends JFrame implements TreeSelectionListener, TreeWillE
         public Color getTextNonSelectionColor(){
             return Color.white;
         }
+/*
+        @Override
+        public  Icon getClosedIcon(){
+            ImageIcon closedIcon = createImageIcon("folder-30.png");
+            return closedIcon;
+        }
+
+        @Override
+        public Icon getOpenIcon(){
+            ImageIcon openIcon = createImageIcon("open-folder-30.png");
+            return openIcon;
+        }
+
+        @Override
+        public Icon getLeafIcon(){
+            ImageIcon closedIcon = createImageIcon("folder-30.png");
+            return closedIcon;
+        }
+
+        @Override
+        public  Icon getDefaultOpenIcon(){
+            ImageIcon dOpenIcon = createImageIcon("arrow-down-15.png");
+            return dOpenIcon;
+
+        }*/
 
         @Override
         public Component getTreeCellRendererComponent(final JTree tree, final Object value, final boolean sel, final boolean expanded, final boolean leaf, final int row, final boolean hasFocus) {
@@ -214,6 +258,65 @@ public class FileTree extends JFrame implements TreeSelectionListener, TreeWillE
             final DefaultMutableTreeNode node = ((DefaultMutableTreeNode) (value));
             this.setText(value.toString());
             return ret;
+        }
+
+        /** Returns an ImageIcon, or null if the path was invalid. */
+        private  ImageIcon createImageIcon(String path) {
+            java.net.URL imgURL = MyCellRenderer.class.getResource(path);
+            if (imgURL != null) {
+                System.out.println("Image URL: " + imgURL);
+
+                return new ImageIcon(imgURL);
+
+            } else {
+                File f = new File(path);
+
+                System.err.println("Couldn't find file: " + path + "\tAbsolute Path: " + f.getAbsolutePath() + "\tExists: " + f.isFile());
+                System.out.println("Image URL: " + imgURL);
+                return null;
+            }
+        }
+    }
+
+    public static final class Icons {
+
+        private static final String COLLAPSED_ICON = "Tree.collapsedIcon";
+        private static final String EXPANDED_ICON = "Tree.expandedIcon";
+        private Icon collapsed;
+        private Icon expanded;
+
+        public static Icons getDefault() {
+            Icons icons = new Icons();
+            icons.collapsed = (Icon) UIManager.get(COLLAPSED_ICON);
+            icons.expanded = (Icon) UIManager.get(EXPANDED_ICON);
+            return new Icons();
+        }
+
+        public  Icons getMetal() {
+            Icons icons = new Icons();
+            try {
+                icons.collapsed = (Icon) createImageIcon("arrow-right-15.png");
+                icons.expanded = (Icon) createImageIcon("arrow-down-15.png");
+            } catch (Exception ex) {
+                ex.printStackTrace(System.err);
+            }
+            return icons;
+        }
+
+        private  ImageIcon createImageIcon(String path) {
+            java.net.URL imgURL = Icons.class.getResource(path);
+            if (imgURL != null) {
+                System.out.println("Image URL: " + imgURL);
+
+                return new ImageIcon(imgURL);
+
+            } else {
+                File f = new File(path);
+
+                System.err.println("Couldn't find file: " + path + "\tAbsolute Path: " + f.getAbsolutePath() + "\tExists: " + f.isFile());
+                System.out.println("Image URL: " + imgURL);
+                return null;
+            }
         }
     }
 
